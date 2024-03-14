f2ae9a526b00cc7a4e6750b2e59f92b533e11d4f
hieu7654321 <157661264+hieu7654321@users.noreply.github.com>
2024-03-14 08:34:54 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String inputFilePath = "input.txt";
+        String outputFilePath = "output.txt";
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+            StringBuilder content = new StringBuilder();
+            String dong;
+            while ((dong = reader.readLine()) != null) {
+                content.append(dong).append("\n");
+            }
+            reader.close();
 
-        Bài làm viết tại đây
+            String replacedContent = content.toString().replaceAll("Nha Trang", "Vũng Tàu");
 
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
+            writer.write(replacedContent);
+            writer.close();
+
+            System.out.println("Đã thay thế và ghi nội dung vào tệp " + outputFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
